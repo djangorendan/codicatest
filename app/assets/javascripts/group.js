a6aa5c870749ec14562c278f7040cd37efcc4d0b
railsdreamteam <ninjarendan@gmail.com>
2020-06-14 13:42:12 +0300

    debug

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,17 +1,17 @@
 $('.owl-carousel').owlCarousel({
-    loop: false,
-    margin: 30,
-    nav: false,
-    dots: false,
-    responsive: {
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:4
-        }
+  loop: false,
+  margin: 30,
+  nav: false,
+  dots: false,
+  responsive: {
+    0:{
+      items:2
+    },
+    1000:{
+      items:3
+    },
+    1200:{
+      items:4
     }
+  }
 })
